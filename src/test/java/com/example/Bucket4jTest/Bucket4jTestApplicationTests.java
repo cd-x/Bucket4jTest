e84e41de1fc9c77dd5a82ddd7dc9d3a684f4de61
cd-x <whowantsthisemailid@gmail.com>
2024-11-30 16:01:24 +0530

    intial commit

---
+++ src/test/java/com/example/Bucket4jTest/Bucket4jTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Bucket4jTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bucket4jTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
